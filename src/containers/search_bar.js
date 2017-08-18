b01579ef2f085fb1ab99c9a43f6d1527a2396365
Greg Duncan <gregdd@outlook.com>
2017-08-18 15:23:22 +1000

    added axios

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchWeather } from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor (props) {
     super (props)
     this.state = {term: ''}
@@ -9,9 +12,17 @@ export default class SearchBar extends Component {
     console.log(event.target.value)
     this.setState({term: event.target.value})
   }
+  onFormSubmit = (event) => {
+    event.preventDefault()
+
+    // we need to fetch weather data
+    this.props.fetchWeather(this.state.term)
+    this.setState({term: ''})
+  }
+
   render () {
     return (
-      <form className='input-group'>
+      <form onSubmit={this.onFormSubmit} className='input-group'>
         <input 
           placeholder='Get a 5 day forcast in your favorite cities'
           className='form-control'
@@ -25,3 +36,9 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
