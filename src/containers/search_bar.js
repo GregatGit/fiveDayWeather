efa0e574ff65fd22e2ca11c89aa43c1573e9a311
Greg Duncan <gregdd@outlook.com>
2017-08-18 23:01:17 +1000

    add sparklines

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,7 +9,7 @@ class SearchBar extends Component {
     this.state = {term: ''}
   }
   onInputChange = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     this.setState({term: event.target.value})
   }
   onFormSubmit = (event) => {
